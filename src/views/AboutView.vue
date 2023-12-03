bb6855b333d72e0aab0d83da51eab39a0f4da2f7
k1audiaa <klaudia.puchaczewski@gmx.de>
2023-12-03 13:41:07 +0100

    Data from backend are loaded

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,45 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <h1>Welcome to TTTGame</h1>
+  <div class="container">
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+      <div class="col" v-for="user in users" :key="user.id">
+        <div class="card h-100">
+          <div class="card-body">
+            <h5 class="card-title">{{ user.username }}</h5>
+            <p class="card-text">
+              Welcome {{ user.username }}!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'AboutView',
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted() {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch('http://localhost:8081/api/users', requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+  }
+}
+
+</script>
+
+
 
 <style>
 @media (min-width: 1024px) {
